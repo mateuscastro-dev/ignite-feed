727aceed75e54841e974c304e5bb28746a4433d1
mateuscastro-dev <mateuscastro.dev@gmail.com>
2022-07-15 22:29:35 -0300

    page layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
 import './styles/global.css'
+import css from './App.module.css'
 
 import { Header } from './components/Header'
 
 export function App() {
-  return <Header />
+  return (
+    <>
+      <Header />
+
+      <div className={css.wrapper}>
+        <aside>
+          <p>sidebar</p>
+        </aside>
+
+        <main>
+          <p>posts</p>
+        </main>
+      </div>
+    </>
+  )
 }
